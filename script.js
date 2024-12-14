57c4c4662094033592b84d8825ada99d84a7515e
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-13 19:35:32 -0800

    Create IIFE's for gameboard array and factory functions for player and gameflow

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const log = console.log
-log("Start")
+// NOTE: minimize globals, use factory functions & IIFE's, prototype inheritance, Object.create(), Scope & Closure, private vs public
+    const log = console.log
 
-// store gameboard as array 
+// initializze gameboard as array 
+const Gameboard = (function () {
+    const initializeGameboard = [];
+    return function () {
+        return initializeGameboard;
+    }
+})();
 
+// factory function to create new players
+function Player(name, marker) {
+    return {name, marker};
+}
+    
+    // example store players in objects
+    const player1 = Player("Jim", "Circle")
+    log(player1)
 
-
-// store players in objects
-
-
+    const player2 = Player("Willis", "Cross")
+    log(player2)
 
 // object to control flow of game
+function gameFlow(playFirst, playSecond) {
+    return {playFirst, playSecond}
+}
+    //example
+    let player1Marker = "Circle"
+    let player2Marker = "Cross"
+
+    const roundOne = gameFlow(player1Marker, player2Marker)
+    log(roundOne)
