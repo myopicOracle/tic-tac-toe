506bcb8bed0f48017b209ec3a317d20416a6d113
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-14 17:16:54 -0800

    Add functionality: place marker, multiple rounds, win condition, announce
    Create factory function that takes a player's choice of board location and
    player identity, and outputs 2 key:value pair objects to replace corresponding
    grid item in gameBoard object array. Memory stored so that with each call of
    function, previous change not overwritten.
    
    Win wondition also created using IF, $$, || - extremely repetitive and manual,
    need to dry code and clean up logic. Currently works and will alert when a
    3 across, 3 down, or 3 diagonal win condition is met.

--- script.js
+++ script.js
@@ -1,32 +1,113 @@
 // NOTE: minimize globals, use factory functions & IIFE's, prototype inheritance, Object.create(), Scope & Closure, private vs public
 const log = console.log
 
-// initializze gameboard as array - IIFE wrapped Factory Function
-const gameBoardX = (function() {
+// initializze gameboard array as IIFE wrapped Factory Function
+const createBoard = (function() {
 
-    const boardArrayX = [];
+    const array = [];
 
     for (let i = 0; i < 9; i++) {
         let number = 1;
-        let positionArray = [];
+        let locationArray = [];
         while(number < 10) {
-            const squareNumber = `square${number}`;
-            positionArray.push(squareNumber);
+            const locationNumber = `square${number}`;
+            locationArray.push(locationNumber);
             number++;
         }
-        const square = positionArray[i];
-        const gameBoardPosition = boardArrayX[i];
-        const obj = {[square]: gameBoardPosition};
-        boardArrayX.push(obj);
+        const location = locationArray[i];
+        const marker = null;
+        const obj = {location, marker};
+        array.push(obj);
     }
-
+ 
     return function() {
-        return boardArrayX;
+        return array;
     }
 
 })();
 
-log(gameBoardX)
+const gameBoard = createBoard()
+console.log(gameBoard)
+
+
+// define marker position for each play
+const placeMarker = function (player, position) {
+
+    const playerWhoIs = player;
+        // prompt("Enter 'playerA' or 'playerB'.");
+    const playerInput = position;
+        // prompt("Please select your marker position. Enter number from 1-9.", "1");
+
+    const index = playerInput - 1;
+    const location = `square${playerInput}`;
+    const marker = (playerWhoIs === "playerA") ? "x" : "o";
+
+    return gameBoard[index] = {location, marker};
+
+};
+// create function to check win condition
+function checkWin() {
+
+    if (
+        ((gameBoard[0].marker !== null && gameBoard[0].marker === gameBoard[1].marker) && (gameBoard[1].marker !== null && gameBoard[1].marker === gameBoard[2].marker)) ||
+        ((gameBoard[3].marker !== null && gameBoard[3].marker === gameBoard[4].marker) && (gameBoard[4].marker !== null && gameBoard[4].marker === gameBoard[5].marker)) ||
+        ((gameBoard[6].marker !== null && gameBoard[6].marker === gameBoard[7].marker) && (gameBoard[7].marker !== null && gameBoard[7].marker === gameBoard[8].marker)) ||
+        ((gameBoard[0].marker !== null && gameBoard[0].marker === gameBoard[3].marker) && (gameBoard[3].marker !== null && gameBoard[3].marker === gameBoard[6].marker)) ||
+        ((gameBoard[1].marker !== null && gameBoard[1].marker === gameBoard[4].marker) && (gameBoard[4].marker !== null && gameBoard[4].marker === gameBoard[7].marker)) ||
+        ((gameBoard[2].marker !== null && gameBoard[2].marker === gameBoard[5].marker) && (gameBoard[5].marker !== null && gameBoard[5].marker === gameBoard[8].marker)) ||
+        ((gameBoard[0].marker !== null && gameBoard[0].marker === gameBoard[4].marker) && (gameBoard[4].marker !== null && gameBoard[4].marker === gameBoard[8].marker)) ||
+        ((gameBoard[2].marker !== null && gameBoard[2].marker === gameBoard[4].marker) && (gameBoard[4].marker !== null && gameBoard[4].marker === gameBoard[6].marker))
+    ) { 
+        return "You Have Won The GAME!";
+    } else return "No Winner";
+
+}
+
+    placeMarker("playerA", 5)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerB", 2))
+    placeMarker("playerB", 2)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerA", 7))
+    placeMarker("playerA", 7)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerB", 1))
+    placeMarker("playerB", 1)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerA", 4))
+    placeMarker("playerA", 4)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerB", 3))
+    placeMarker("playerB", 3)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerA", 8))
+    placeMarker("playerA", 8)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerB", 9))
+    placeMarker("playerB", 9)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+    // console.log(placeMarker("playerA", 6))
+    placeMarker("playerA", 6)
+    console.log(gameBoard);
+    console.log(checkWin())
+
+
 
 // factory function to create new players
 function Player(name, marker) {
