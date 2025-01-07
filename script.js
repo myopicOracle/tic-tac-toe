36b67c66ac8c48ffe9ae165a3e2b65b4fccc72ea
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2025-01-07 12:34:18 -0800

    Add all game logic up to the final feature - check win condition
    Current structure is 1 Global Variable, 1 Class, 1 Variable
    Assignment to new Class Object.
    
    This commit reflects addition of following features:
    
    1. Change current player marker by clicking player button
    2. Place marker on corresponding game tile with UI displayed

--- script.js
+++ script.js
@@ -34,32 +34,50 @@ class PlayerTwo extends PlayMove {
 
 // Create app gameplay logic
 class GameLogic {
+    // Page structure DOM
     anchor = document.querySelector("div.anchor")
     gameBoard = document.createElement("div")
     buttonPanel = document.createElement("div")
     buttonOne = document.createElement("button")
     buttonTwo = document.createElement("button")
 
+    // Gameboard DOM
+    tile1 = document.getElementById("1")
+    tile2 = document.getElementById("2")
+    tile3 = document.getElementById("3")
+    tile4 = document.getElementById("4")
+    tile5 = document.getElementById("5")
+    tile6 = document.getElementById("6")
+    tile7 = document.getElementById("7")
+    tile8 = document.getElementById("8")
+    tile9 = document.getElementById("9")
+
+    // Player Logic
+    playerOneMarker = "X"
+    playerTwoMarker = "O"
+
+    // Called immediately on page load
     constructor() {
-        // this.gameBoardArray = [] // moved to global scope
+        this.marker = ""
         this._renderBoard()
         this._renderButtons()
-        this._playerOneChoice()
-        this._playerTwoChoice()
+        this.buttonOne.addEventListener("click", this._playerOneChoice.bind(this))
+        this.buttonTwo.addEventListener("click", this._playerTwoChoice.bind(this))
+        this.gameBoard.addEventListener("click", this._placeMarker.bind(this))
     }
 
     _renderBoard() {
         this.gameBoard.setAttribute("class", "gameBoard")
         this.gameBoard.innerHTML = `
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
-            <div class="blankTile gameBoard"></div>
+            <div id="1" class="blankTile gameBoard"></div>
+            <div id="2" class="blankTile gameBoard"></div>
+            <div id="3" class="blankTile gameBoard"></div>
+            <div id="4" class="blankTile gameBoard"></div>
+            <div id="5" class="blankTile gameBoard"></div>
+            <div id="6" class="blankTile gameBoard"></div>
+            <div id="7" class="blankTile gameBoard"></div>
+            <div id="8" class="blankTile gameBoard"></div>
+            <div id="9" class="blankTile gameBoard"></div>
             `
         this.anchor.appendChild(this.gameBoard)
     }
@@ -77,13 +95,19 @@ class GameLogic {
     }
 
     _playerOneChoice() {
-        this.buttonOne.addEventListener("click", (e) => {
-            e.target.someFunction()
-        })
+        this.marker = "X"
+        console.log(this.marker)
     }
 
     _playerTwoChoice() {
-
+        this.marker = "O"
+        console.log(this.marker)
+    }
+    
+    _placeMarker() {
+        this.gameBoard.addEventListener("click", (e) => {
+            e.target.textContent = this.marker
+        })  
     }
 
     _checkWinCondition() {
